57c940d6f0142388edd7f380bcdc03a21f84c2c7
Hannah <hanniesutton@gmail.com>
2019-04-19 18:43:09 +0000

    Add body to emails that are sent out

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
 
   def show
-    cartSession = User.find(session(:user_id))
+    # cartSession = User.find(session(:user_id))
   end
 
   def add_item
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,9 @@ class UserMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
 
   def order_email(email, order)
-    @user = email
+    @email = email
     @order = order
-    @url = 'http://example.com/login'
-    mail(to: @user, subject: 'Your Jungle Order #{order.id}')
+    mail(to: email, subject: "Your Jungle Order #{order.id}")
   end
 
 end
